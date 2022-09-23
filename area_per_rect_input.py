3235e3a6fb78ec675c90f23aff87f84719adbb81
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-23 01:15:44 +0000

    added area_per_rect_input.py

---
+++ area_per_rect_input.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# Created By: Your Frankie fox 
+# Date: Sept 22, 2022
+# This program calculates the area & perimeter of a rectangle
+
+def main ():
+    print("For a rectangle with a length of 3 and width of 2:")
+    print("The area is :{}cm^2".format(3*2))
+    print("The perimeter is :{}cm".format(2*(3+2)))
+    print("For a rectangle with a length of 6 and width of 10:")
+    print("The area is :{}cm^2".format(6*10))
+    print("The perimeter is :{}cm".format(2*(6+10)))
+    print("For a rectangle with a length of 20 and width of 5:")
+    print("The area is :{}cm^2".format(20*5))
+    print("The perimeter is :{}cm".format(2*(20+5)))
+
+if __name__ == "__main__":
+    main()
